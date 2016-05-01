394be0e5aaa676f16842c21b24ef811923c464d9
rightfold <rightfold@gmail.com>
2016-05-01 19:00:33 +0200

    Implement I/O actions

---
+++ src/main/hack/IO.hh
@@ -0,0 +1,24 @@
+<?hh // strict
+namespace FH;
+
+final class IO<T> {
+    public function __construct(private (function(): T) $action) { }
+
+    public function unsafePerform(): T {
+        return call_user_func($this->action);
+    }
+
+    public function map<TM>((function(T): TM) $f): IO<TM> {
+        return new IO(function() use($f) {
+            $x = $this->unsafePerform();
+            return $f($x);
+        });
+    }
+
+    public function bind<TM>((function(T): IO<TM>) $f): IO<TM> {
+        return new IO(function() use($f) {
+            $x = $this->unsafePerform();
+            return $f($x)->unsafePerform();
+        });
+    }
+}
