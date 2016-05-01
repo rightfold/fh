e474be2b5d43463ed7ea791268e6af8c8aff933b
rightfold <rightfold@gmail.com>
2016-05-01 19:48:31 +0200

    Test Option#map

--- src/test/hack/FH/OptionTest.hh
+++ src/test/hack/FH/OptionTest.hh
@@ -12,4 +12,9 @@ class OptionTest extends PHPUnit_Framework_TestCase {
         $this->assertSame(1, Option::none() ->optionL(() ==> 1, F::id()));
         $this->assertSame(2, Option::some(2)->optionL(() ==> 1, F::id()));
     }
+
+    public function testMap(): void {
+        $this->assertSame(1, Option::none() ->map($x ==> $x + 1)->option(1, F::id()));
+        $this->assertSame(3, Option::some(2)->map($x ==> $x + 1)->option(1, F::id()));
+    }
 }
