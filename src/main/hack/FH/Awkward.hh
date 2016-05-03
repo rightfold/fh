09fde830c4fb8dd25aeec8a2d611be16af127082
rightfold <rightfold@gmail.com>
2016-05-03 20:46:51 +0200

    Compact Awkward internal representation

--- src/main/hack/FH/Awkward.hh
+++ src/main/hack/FH/Awkward.hh
@@ -4,31 +4,30 @@ namespace FH;
 final class Awkward<+TError, +TOk> {
     private function __construct(
         private bool $isOk,
-        private TError $error,
-        private TOk $ok
+        private mixed $value
     ) { }
 
     public static function error(TError $error): Awkward<TError, TOk> {
-        return new Awkward(false, $error, /* UNSAFE_EXPR */ null);
+        return new Awkward(false, $error);
     }
 
     public static function ok(TOk $ok): Awkward<TError, TOk> {
-        return new Awkward(true, /* UNSAFE_EXPR */ null, $ok);
+        return new Awkward(true, $ok);
     }
 
     public function awkward<TR>((function(TError): TR) $onError, (function(TOk): TR) $onOk): TR {
         if ($this->isOk) {
-            return $onOk($this->ok);
+            return $onOk(/* UNSAFE_EXPR */ $this->value);
         } else {
-            return $onError($this->error);
+            return $onError(/* UNSAFE_EXPR */ $this->value);
         }
     }
 
     public function map<TM>((function(TOk): TM) $f): Awkward<TError, TM> {
         if ($this->isOk) {
-            return Awkward::ok($f($this->ok));
+            return Awkward::ok($f(/* UNSAFE_EXPR */ $this->value));
         } else {
-            return Awkward::error($this->error);
+            return Awkward::error(/* UNSAFE_EXPR */ $this->value);
         }
     }
 }
