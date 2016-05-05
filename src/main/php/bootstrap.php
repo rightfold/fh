e48237ed64191f911ba76fe8c15faaacfb9c6fc1
rightfold <rightfold@gmail.com>
2016-05-05 19:27:13 +0200

    Rewrite IO in Rose

--- src/main/php/bootstrap.php
+++ src/main/php/bootstrap.php
@@ -1,3 +1,11 @@
 <?php
 $GLOBALS['HACKLIB_ROOT'] = '/opt/hack/usr/share/hhvm/hack/hacklib/hacklib.php';
+function fun($fun) { return $fun; }
+function inst_meth($inst, $meth) { return [$inst, $meth]; }
+function class_meth($class, $meth) { return [$class, $meth]; }
+function meth_caller($class, $meth) {
+    return function($self, ...$args) use($meth) {
+        return $self->$meth(...$args);
+    };
+}
 require(__DIR__ . '/../../../vendor/autoload.php');
