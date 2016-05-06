5a9348eeead53216087babf6617638e3de6faaf0
rightfold <rightfold@gmail.com>
2016-05-06 14:13:12 +0200

    Implement Awkward#bind

--- src/main/hack/FH/Awkward.hh
+++ src/main/hack/FH/Awkward.hh
@@ -1,7 +1,7 @@
 <?hh // strict
 namespace FH;
 
-final class Awkward<+TError, +TOk> {
+final class Awkward<TError, TOk> {
     private function __construct(
         private bool $isOk,
         private mixed $value
@@ -26,6 +26,14 @@ final class Awkward<+TError, +TOk> {
     public function map<TM>((function(TOk): TM) $f): Awkward<TError, TM> {
         if ($this->isOk) {
             return Awkward::ok($f(/* UNSAFE_EXPR */ $this->value));
+        } else {
+            return Awkward::error(/* UNSAFE_EXPR */ $this->value);
+        }
+    }
+
+    public function bind<TOk2>((function(TOk): Awkward<TError, TOk2>) $f): Awkward<TError, TOk2> {
+        if ($this->isOk) {
+            return $f(/* UNSAFE_EXPR */ $this->value);
         } else {
             return Awkward::error(/* UNSAFE_EXPR */ $this->value);
         }
