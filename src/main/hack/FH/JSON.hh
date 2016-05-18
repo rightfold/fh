32508a428b01761141df481e1250c5b3eb504eb5
Radek Slupik <rightfold@gmail.com>
2016-05-18 16:53:31 +0200

    Allow null values in JSON factories

--- src/main/hack/FH/JSON.hh
+++ src/main/hack/FH/JSON.hh
@@ -6,21 +6,21 @@ use DateTimeInterface;
 final class JSON {
     private function __construct(private mixed $value) { }
 
-    public static function null  ():          JSON { return new JSON(null); }
-    public static function bool  (bool $v):   JSON { return new JSON($v); }
-    public static function num   (num $v):    JSON { return new JSON($v); }
-    public static function string(string $v): JSON { return new JSON($v); }
+    public static function null  ():           JSON { return new JSON(null); }
+    public static function bool  (?bool $v):   JSON { return new JSON($v); }
+    public static function num   (?num $v):    JSON { return new JSON($v); }
+    public static function string(?string $v): JSON { return new JSON($v); }
 
-    public static function dateTime(DateTimeInterface $v): JSON {
-        return new JSON($v->format(DateTime::ISO8601));
+    public static function dateTime(?DateTimeInterface $v): JSON {
+        return new JSON($v === null ? null : $v->format(DateTime::ISO8601));
     }
 
-    public static function array_(ImmVector<JSON> $vs): JSON {
-        return new JSON($vs->map(function($v) { return $v->value; })->toArray());
+    public static function array_(?ImmVector<JSON> $vs): JSON {
+        return new JSON($vs === null ? null : $vs->map(function($v) { return $v->value; })->toArray());
     }
 
-    public static function object(ImmMap<string, JSON> $vs): JSON {
-        return new JSON((object)$vs->map(function($v) { return $v->value; })->toArray());
+    public static function object(?ImmMap<string, JSON> $vs): JSON {
+        return new JSON($vs === null ? null : (object)$vs->map(function($v) { return $v->value; })->toArray());
     }
 
     public function encode(): string {
